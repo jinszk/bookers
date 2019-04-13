f65cb66f81cd854851c08298f72d08da84fbdee4
jinszk <jinszk1991@gmail.com>
2019-04-13 20:47:28 +0900

    [Update]4-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/" => "book#top"
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   resources :blogs
+   resources :books
 end
