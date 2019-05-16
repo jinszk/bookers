356f3b5e530b49f96814b739ebcff3b3cb4f85a8
jinszk <jinszk1991@gmail.com>
2019-05-16 17:28:05 +0900

    [Finish] 4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,40 @@
 class BooksController < ApplicationController
-  def top
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    flash[:notice] = "successfully"
+    redirect_to book_path(book.id)
   end
 
   def show
-  end
-
-  def index
-  end
-
-  def new
+  	@book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "successfully"
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
